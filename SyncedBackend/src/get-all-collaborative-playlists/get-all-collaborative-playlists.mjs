176ea2d3a10cbef2696e69f3f570851f51295ff0
Jack Hurt <jackwhurt@gmail.com>
2023-11-06 13:37:54 +0000

    Restructured so each resource has its own cloudformation template

--- SyncedBackend/src/get-all-collaborative-playlists/get-all-collaborative-playlists.mjs
+++ SyncedBackend/src/get-all-collaborative-playlists/get-all-collaborative-playlists.mjs
@@ -12,7 +12,7 @@ const tableName = process.env.SAMPLE_TABLE;
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
-export const getAllItemsHandler = async (event) => {
+export const getAllCollaborativePlaylistsHandler = async (event) => {
     if (event.httpMethod !== 'GET') {
         throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
     }
