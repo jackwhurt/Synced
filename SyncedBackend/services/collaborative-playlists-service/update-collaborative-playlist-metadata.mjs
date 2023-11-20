c12a036e8dfe487406f48207c1d0c08eae3d964a
Jack Hurt <jackwhurt@gmail.com>
2023-11-20 17:45:29 +0000

    Update cp metadata logic

---
+++ SyncedBackend/services/collaborative-playlists-service/update-collaborative-playlist-metadata.mjs
@@ -0,0 +1,79 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+const tableName = process.env.PLAYLISTS_TABLE;
+
+export const updateCollaborativePlaylistHandler = async (event) => {
+    console.info('received:', event);
+
+    if (!event.body) {
+        return { statusCode: 400, body: JSON.stringify({ message: 'Missing body' }) };
+    }
+
+    const { playlist } = JSON.parse(event.body);
+
+    if (!playlist || !playlist.id) {
+        return { statusCode: 400, body: JSON.stringify({ message: 'Missing playlist ID' }) };
+    }
+
+    const claims = event.requestContext.authorizer?.claims;
+    if (!claims) {
+        return { statusCode: 401, body: JSON.stringify({ message: 'Unauthorised' }) };
+    }
+
+    const cognitoUserId = claims['sub'];
+    const timestamp = new Date().toISOString();
+
+    let updateExpression = "set #updatedAt = :updatedAt";
+    let expressionAttributeNames = { "#updatedAt": "updatedAt" };
+    let expressionAttributeValues = { ":updatedAt": timestamp };
+
+    // Update only specific fields: title, description, coverImageUrl
+    const fieldsToUpdate = ['title', 'description', 'coverImageUrl'];
+    fieldsToUpdate.forEach((field) => {
+        if (playlist[field] !== undefined) {
+            updateExpression += `, #${field} = :${field}`;
+            expressionAttributeNames[`#${field}`] = field;
+            expressionAttributeValues[`:${field}`] = playlist[field];
+        }
+    });
+
+    try {
+        await ddbDocClient.send(new UpdateCommand({
+            TableName: tableName,
+            Key: {
+                PK: `cp#${playlist.id}`,
+                SK: `metadata`
+            },
+            UpdateExpression: updateExpression,
+            ConditionExpression: "#createdBy = :createdBy",
+            ExpressionAttributeNames: {
+                ...expressionAttributeNames,
+                "#createdBy": "createdBy"
+            },
+            ExpressionAttributeValues: {
+                ...expressionAttributeValues,
+                ":createdBy": cognitoUserId
+            }
+        }));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Playlist updated successfully' })
+        };
+    } catch (err) {
+        if (err.name === 'ConditionalCheckFailedException') {
+            return {
+                statusCode: 403,
+                body: JSON.stringify({ message: 'Unauthorised to update this playlist' })
+            };
+        }
+        console.error("Error", err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error updating the collaborative playlist' })
+        };
+    }
+};
