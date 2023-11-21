f9dbdc41b9ef0fa2efd2f3d6b40a21baef7ce129
Jack Hurt <jackwhurt@gmail.com>
2023-11-21 15:20:12 +0000

    Added retries to spotify token refresh and sns topic

--- SyncedBackend/services/token-service/update-spotify-dev-token.mjs
+++ SyncedBackend/services/token-service/update-spotify-dev-token.mjs
@@ -7,49 +7,64 @@ const ssmClient = new SSMClient({});
 const dynamoDBClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 const tokensTable = process.env.TOKENS_TABLE;
+const snsTopic = process.env.LAMBDA_FAILURE_TOPIC;
 
 export const updateSpotifyDevTokenHandler = async (event) => {
-    try {
-        // Retrieve Client ID and Client Secret from Parameter Store
-        const clientId = await getParameter('spotifyClientId');
-        const clientSecret = await getParameter('spotifyClientSecret');
+    let retryCount = 0;
 
-        // Retrieve Refresh Token from DynamoDB
-        const refreshTokenData = await dynamoDBClient.send(new GetCommand({
-            TableName: tokensTable,
-            Key: { token_id: 'SpotifyDev' }
-        }));
-        const refreshToken = refreshTokenData.Item.refresh_token;
+    while (retryCount < MAX_RETRIES) {
+        try {
+            // Retrieve Client ID and Client Secret from Parameter Store
+            const clientId = await getParameter('spotifyClientId');
+            const clientSecret = await getParameter('spotifyClientSecret');
 
-        // Prepare the request for token refresh
-        const tokenRefreshUrl = 'https://accounts.spotify.com/api/token';
-        const authHeader = 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
-        const body = new URLSearchParams();
-        body.append('grant_type', 'refresh_token');
-        body.append('refresh_token', refreshToken);
+            // Retrieve Refresh Token from DynamoDB
+            const refreshTokenData = await dynamoDBClient.send(new GetCommand({
+                TableName: tokensTable,
+                Key: { token_id: 'SpotifyDev' }
+            }));
+            const refreshToken = refreshTokenData.Item.refresh_token;
 
-        // Make the request to Spotify
-        const response = await axios.post(tokenRefreshUrl, body, {
-            headers: { 
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': authHeader
-            }
-        });
+            // Prepare the request for token refresh
+            const tokenRefreshUrl = 'https://accounts.spotify.com/api/token';
+            const authHeader = 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+            const body = new URLSearchParams();
+            body.append('grant_type', 'refresh_token');
+            body.append('refresh_token', refreshToken);
 
-        const newAccessToken = response.data.access_token;
-        const expiresIn = response.data.expires_in;
-        const newRefreshToken = response.data.refresh_token || refreshToken;
+            // Make the request to Spotify
+            const response = await axios.post(tokenRefreshUrl, body, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': authHeader
+                }
+            });
 
-        // Update the access token (and refresh token, if new one is provided) in DynamoDB
-        await updateAccessToken(tokensTable, 'SpotifyDev', newAccessToken, expiresIn, newRefreshToken);
+            const newAccessToken = response.data.access_token;
+            const expiresIn = response.data.expires_in;
+            const newRefreshToken = response.data.refresh_token || refreshToken;
 
-        return { statusCode: 200, body: JSON.stringify({ message: 'Token refreshed successfully' }) };
-    } catch (apiError) {
-        console.error('Error refreshing token:', apiError);
-        
-        return { statusCode: 500, body: JSON.stringify({ message: 'Failed to refresh token' }) };
+            // Update the access token (and refresh token, if new one is provided) in DynamoDB
+            await updateAccessToken(tokensTable, 'SpotifyDev', newAccessToken, expiresIn, newRefreshToken);
+
+            return { statusCode: 200, body: JSON.stringify({ message: 'Token refreshed successfully' }) };
+        } catch (apiError) {
+            console.error('Attempt', retryCount + 1, 'failed:', apiError);
+            retryCount++;
+
+            if (retryCount >= MAX_RETRIES) {
+                // Handle the case where all retries have failed
+                console.error('All attempts failed.');
+                // Send alert
+                await sns.publish({
+                    Message: 'Update Spotify Dev Token function failed after max retries',
+                    TopicArn: snsTopic
+                }).promise();
+                    return { statusCode: 500, body: JSON.stringify({ message: 'Failed to refresh token after retries' }) };
+                }
+        }
     }
-};
+}
 
 async function getParameter(name) {
     const parameter = await ssmClient.send(new GetParameterCommand({ Name: name, WithDecryption: true }));
@@ -59,7 +74,7 @@ async function getParameter(name) {
 
 async function updateAccessToken(tableName, primaryKeyValue, accessToken, expiresIn, newRefreshToken = null) {
     const updateExpression = 'set access_token = :a, ExpiresAt = :e' + (newRefreshToken ? ', refresh_token = :r' : '');
-    
+
     const expressionAttributeValues = {
         ':a': accessToken,
         ':e': new Date().getTime() + expiresIn * 1000
