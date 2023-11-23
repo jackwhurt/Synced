137365d3bbc88db37b6f674807cbb3cd380eba5d
Jack Hurt <jackwhurt@gmail.com>
2023-11-23 19:24:26 +0000

    Init frontend

---
+++ SyncedFrontend/SyncedFrontend/SyncedFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncedFrontendApp.swift
+//  SyncedFrontend
+//
+//  Created by Jack Hurt on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SyncedFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
