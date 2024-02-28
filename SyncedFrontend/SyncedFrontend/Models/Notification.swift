c829846273f8e00fff99c9553c9020b657231c23
Jack Hurt <jackwhurt@gmail.com>
2024-02-28 13:08:58 +0000

    Add collaborator functionality done

--- SyncedFrontend/SyncedFrontend/Models/Notification.swift
+++
@@ -1,7 +0,0 @@
-struct NotificationMetadata: Codable, Hashable {
-    let message: String
-    let createdBy: String
-    let notificationId: String
-    let playlistId: String?
-    let createdAt: String?
-}
